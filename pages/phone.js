662bc8f2de526d5e761afda2c32bddc09f4f3304
M1-Masa <masa455.wv@gmail.com>
2022-05-26 13:10:29 +0900

    add

--- pages/phone.js
+++ pages/phone.js
@@ -1,18 +1,19 @@
 export async function getServerSideProps() {
-  const WEB_APP_URL ="https://script.google.com/macros/s/AKfycbxuIL-slPOq3eF5LVQ8Ss0QJAZ33ccH8FZr1Z8a6EfIP5JiIs3deA-exPs7437g-j5b/exec";
-  const response = await fetch(WEB_APP_URL);
+  /** GASのWEBアプリケーションURL */
+const WEB_APP_URL ="https://script.google.com/macros/s/AKfycbxuIL-slPOq3eF5LVQ8Ss0QJAZ33ccH8FZr1Z8a6EfIP5JiIs3deA-exPs7437g-j5b/exec";
 
-  const data = JSON.parse(JSON.stringify(response));
-  
-  // const data = await response.json();
-  console.log(data);
-  return {
-    props: {
-      props: data,
-    },
-  }
+const response = await fetch(WEB_APP_URL);
 
-}
+const data = await response.json();
+console.log(data);
+return {
+      props: {
+        props: data,
+      },
+    }
+
+};
+
 const product = ({ props }) => {
   console.log(props);
   return (
