a704c9195052270dbcbfbcee1b9848f204eda172
M1-Masa <masa455.wv@gmail.com>
2022-05-26 17:26:26 +0900

    qqq

--- pages/api/test.js
+++ pages/api/test.js
@@ -5,7 +5,7 @@
 
 export default async (req, res) => {
   try {
-    res.status(200).json({ result: `「${body?.message}」を受け取りました！` });
+    res.status(200).json({ result: `を受け取りました！` });
     
   } catch (e) {
     res.status(500).json({ result: `エラー！` });
