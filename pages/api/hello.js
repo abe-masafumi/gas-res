cdfa5dc26d1c65433a1fcf4d63c2af2ce4f3bbda
M1-Masa <masa455.wv@gmail.com>
2022-05-26 12:11:15 +0900

    add:test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+
+/** リクエストに対して返却するデータ */
+
+export default async (req, res) => {
+  console.log(req);
+  try {
+    res.status(200).json({ result: `Next.jsから送信されました!` });
+  } catch (e) {
+    res.status(500).json({ result: `エラー！` });
+  }
+};
+
+/** GASのWEBアプリケーションURL */
+
+// const getDataFromGAS = async() => {
+
+// };
