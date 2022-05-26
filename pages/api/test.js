283710e54310a64ef2038c59f6f7b01da06951a4
M1-Masa <masa455.wv@gmail.com>
2022-05-26 17:08:39 +0900

    eee

---
+++ pages/api/test.js
@@ -0,0 +1,12 @@
+
+/** リクエストに対して返却するデータ */
+
+/** 想定されるリクエストパラメータ */
+
+export default async (req, res) => {
+  try {
+    res.status(200).json({ result: `「${body?.message}」を受け取りました！` });
+  } catch (e) {
+    res.status(500).json({ result: `エラー！` });
+  }
+};
