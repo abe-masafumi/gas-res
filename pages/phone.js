82b954f8bd163b0b0eb0a8c032048b21faac5d2b
M1-Masa <masa455.wv@gmail.com>
2022-05-26 13:36:58 +0900

    add

--- pages/phone.js
+++ pages/phone.js
@@ -1,7 +1,7 @@
-export async function getServerSideProps({context}) {
+export async function getServerSideProps(context) {
   console.log(context);
   /** GASのWEBアプリケーションURL */
-const WEB_APP_URL ="https://script.google.com/macros/s/AKfycbxuIL-slPOq3eF5LVQ8Ss0QJAZ33ccH8FZr1Z8a6EfIP5JiIs3deA-exPs7437g-j5b/exec";
+const WEB_APP_URL ="https://script.googleusercontent.com/macros/echo?user_content_key=Bg7hyKFV69uksFmLzuDb1yUB8dXffbEpTMLt-2az8SE_NJhrIHMURQLL6nsnU9SvbgoJMbOXkUGq3gl_Ze6M-xAgcTH9oL_hm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHygPuFLngzF7q2ueWSz0xWqdqSpR82WqICKot89PA2d89Dwc6HU6qcFonY-Q_58fcRGeKnPk8bMqThbrF0vKhM1Gs9BPt8QXg&lib=MHiYzXSUXvtslAeKU6a5bYz4e5fkJzZ6i";
 
 const response = await fetch(WEB_APP_URL);
 
