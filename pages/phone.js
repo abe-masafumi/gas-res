08abab06793babe744b5cf4ba8d3a2f94bc4b648
M1-Masa <masa455.wv@gmail.com>
2022-05-27 15:36:09 +0900

    tt

--- pages/phone.js
+++ pages/phone.js
@@ -1,15 +1,15 @@
 
-export async function getServerSideProps() {
-  const response = await fetch(
-    // 1iayuC-1ZvvJ3loMtgW6Lpnh7_DzS_fc4_eAcLiEvn2s
-    'https://sheets.googleapis.com/v4/spreadsheets/1iayuC-1ZvvJ3loMtgW6Lpnh7_DzS_fc4_eAcLiEvn2s/values/%E3%82%B7%E3%83%BC%E3%83%881?key=AIzaSyArmCbf1YItXgxUXpDkUb6s1LXxfQOoimc'  )
-  const postList = await response.json()
-  return{
-    props: {
-      initPostList: postList
-    }
-  }
-};
+// export async function getServerSideProps() {
+//   const response = await fetch(
+//     // 1iayuC-1ZvvJ3loMtgW6Lpnh7_DzS_fc4_eAcLiEvn2s
+//     'https://sheets.googleapis.com/v4/spreadsheets/1iayuC-1ZvvJ3loMtgW6Lpnh7_DzS_fc4_eAcLiEvn2s/values/%E3%82%B7%E3%83%BC%E3%83%881?key=AIzaSyArmCbf1YItXgxUXpDkUb6s1LXxfQOoimc'  )
+//   const postList = await response.json()
+//   return{
+//     props: {
+//       initPostList: postList
+//     }
+//   }
+// };
 
 
 
