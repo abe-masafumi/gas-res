5850c2614a13f4bcd33fcc22d7025ca34ae0d48c
M1-Masa <masa455.wv@gmail.com>
2022-05-27 12:32:45 +0900

    wwww

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,7 +2,8 @@
 
 export default async (req, res) => {
   try {
-    res.status(200).json({ result: `${JSON.parse(req)}` });
+    res.status(200).json({ result: `www` });
+    // 
   } catch (e) {
     res.status(500).json({ result: `エラー！` });
   }
