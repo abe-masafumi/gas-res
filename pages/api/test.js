3202251db85f963b09192dff93e5ae0580d76259
M1-Masa <masa455.wv@gmail.com>
2022-05-27 13:36:59 +0900

    3333322

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,9 +1,9 @@
 
 
-export default async (body, res) => {
+export default async (req, res) => {
   try {
-    const test = JSON.parse(body)
-    res.status(200).json({ result: `${test}` });
+    const test = JSON.parse(req.body)
+    res.status(200).json({ result: `${test?.values}` });
     // 
   } catch (e) {
     res.status(500).json({ result: `エラー！` });
